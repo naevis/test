a5a864c215ab70ca27bea318c3e78b19aaace579
naevis <88284487+naevis@users.noreply.github.com>
2021-08-03 14:10:41 +0700

    Update Bai3.cpp

--- Bai3.cpp
+++ Bai3.cpp
@@ -9,11 +9,11 @@ int main (){
     scanf ("%f", &dtb);
 if ( dtb >=8 &&  dt>=8 |dv >= 8) {
 	printf ("Hoc luc gioi");
-}if (dtb >= 6.5 && dt >= 6.5 |dv >= 6.5) {
+}else if (dtb >= 6.5 && dt >= 6.5 |dv >= 6.5) {
 	printf ("Hoc luc kha");
-} if (dtb>=5 && dt >=5 | dv >=5){
+}else if (dtb>=5 && dt >=5 | dv >=5){
 	printf ("Hoc luc trung binh");
-} if (dtb<5) {
+} if (dtb<5 && dt < 5 | dv <5) {
 	printf ("Hoc luc yeu");	
 	} if ( dtb>10 | dt > 10 | dv > 10 ) {
 		printf ("Diem khong hop le");
