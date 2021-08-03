52199f6042b224c220f9e176438a128d8210c203
naevis <88284487+naevis@users.noreply.github.com>
2021-08-03 14:21:13 +0700

    Update Bai 2 (loi).cpp

--- Bai 2 (loi).cpp
+++ Bai 2 (loi).cpp
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <math.h>
-
-int main(){
-    int n;
-    printf("\n Nhap n = ");
-    scanf("%d", &n);
-    if(n < 2){
-        printf("%d khong phai so nguyen to", n);
-        
-    int count = 0;
-    for(int i = 2; i <= sqrt(n); i++){
-        if(n % i == 0){
-            count++;
-        }
-    }
-    if(count == 0){
-        printf("\n%d la so nguyen to", n);
-    }else{
-        printf("\n%d khong phai so nguyen to", n);
-    }
-}
+int main() {
+	int n;
+	printf("Nhap n ");
+	scanf("%d", &n);
+	if(n < 2) {
+		printf("%d khong la so nguyen to.\n", n);
+	} else {
+		int i;
+		int dem = 1;
+		for(i = 2; i <= sqrt(n); i++)  {
+			if(n % i == 0) {
+				dem = 0;
+				break;
+			}
+		}
+		if(dem == 1) {
+			printf("%d la so nguyen to.\n", n);
+		} else {
+			printf("%d khong la so nguyen to.\n", n);
+		}
+		
+	}
+	return 0;
 }
